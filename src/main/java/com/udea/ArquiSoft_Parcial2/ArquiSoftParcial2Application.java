172d4eceba677959d1c33f25299891249e7493af
eospgonz10 <estiven.ospinag@udea.edu.co>
2025-07-18 10:14:22 -0500

    primer commit - parcial 2

---
+++ src/main/java/com/udea/ArquiSoft_Parcial2/ArquiSoftParcial2Application.java
@@ -0,0 +1,13 @@
+package com.udea.ArquiSoft_Parcial2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiSoftParcial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiSoftParcial2Application.class, args);
+	}
+
+}
